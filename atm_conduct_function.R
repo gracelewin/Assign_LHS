fe38e96eac1bb995ac61c908c260ec3ef40221f8
gracelewin <glewin@bren.ucsb.edu>
2022-04-25 16:52:29 -0700

    added more to part 3f.

--- atm_conduct_function.R
+++ atm_conduct_function.R
@@ -4,11 +4,6 @@ atm_conduct <- function(kd = 0.7, k0 = 0.1, vm, h) {
   
   zm = h + 200
   
-  # if(zm > h){
-  #    Cat = vm / (6.25 * log((zm - zd) / z0) ^2)
-  #    return(Cat)} 
-  #    else{"zm must be greater than vegitation height"}
-  
   Cat = vm / (6.25 * log((zm - zd) / z0) ^2)
   
   #return(Cat)
